2209bb36ef03a30483bd6eb64250a66160974b92
Gokhan Serin <penmudamu@gmail.com>
2023-07-25 15:50:49 +0300

    Cukes runner completed

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -1,5 +1,25 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = {
+            "html:target/cucumber-report.html",
+            "json:target/cucumber-report.json",
+            "junit:target/cucumber-report.xml",
+            "rerun:target/rerun.txt",
+    },
+        features = "src/test/resources/features",
+        glue = "step_definitions",
+        dryRun = true,
+        tags = "",
+        publish = true
+
+)
+
 public class CukesRunner {
 
 }
