2840eb9226f8375716cbf34fd0286388d1868451
Gokhan Serin <penmudamu@gmail.com>
2023-07-25 14:56:14 +0300

    login page created

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+}
+
+@FindBy(css = "a[class='btnSignIn']")
+    public WebElement loginButton;
+
+
 }
